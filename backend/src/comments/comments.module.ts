cd71d74b1c9d4219e3b64d727fd6860718fd4bc7
kunaldaharwal.h <kunaldaharwal.h@gmail.com>
2025-10-26 14:57:00 +0000

    r5/2/fx

--- backend/src/comments/comments.module.ts
+++ backend/src/comments/comments.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CommentsController],
   providers: [CommentsService]
 })
